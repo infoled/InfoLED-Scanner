d0ec5d0ddfec8eef467e74cb9787fa584f0d1f7a
Jackie Yang <me@jackieyang.me>
2019-03-06 23:05:27 -0800

    Build test for entire folder

--- InfoLED ScannerTests/InfoLED_ScannerTests.swift
+++ InfoLED ScannerTests/InfoLED_ScannerTests.swift
@@ -43,6 +43,57 @@ class TestBufferProcessorDelegate: SampleBufferProcessorDelegate {
 
 }
 
+class TestPacketResult {
+    let frameId: Int
+    let actualPacket: [Int]
+    let BER: Float
+
+    init(frameId: Int, actualPacket: [Int], correctPacket: inout [Int]) {
+        var wrongBits = 0
+        for (actualDigit, correctDigit) in zip(actualPacket, correctPacket) {
+            wrongBits += actualDigit == correctDigit ? 0 : 1
+        }
+        self.BER = Float(wrongBits) / Float(correctPacket.count)
+        self.actualPacket = actualPacket
+        self.frameId = frameId
+    }
+}
+
+let BER_THRESHOLD = Float(0.0)
+
+class TestVideoResult {
+    var packets = [TestPacketResult]()
+    var averageBER: Float!
+    var packetSuccessRate: Float!
+    var totalFrames: Int!
+    var firstPacketFrame: Int? = nil
+
+    func calculateResult(totalFrames: Int, packetLength: Int = HistoryProcessor.totalPacketLength) {
+        let totalPacketCount = totalFrames / packetLength
+        self.totalFrames = totalFrames
+        self.averageBER = packets.reduce(Float(0), { (sum, packetResult) in sum + packetResult.BER}) / Float(packets.count)
+        self.packetSuccessRate = Float(self.packets.count) / Float(totalPacketCount)
+        firstPacketFrame = packets.reduce(totalFrames + 1, { (result, packet) in
+            if packet.BER <= BER_THRESHOLD && packet.frameId < result {
+                return packet.frameId
+            } else {
+                return result
+            }
+        })
+        firstPacketFrame = firstPacketFrame == totalFrames + 1 ? nil : firstPacketFrame
+    }
+
+    func printDescription() {
+        print("""
+            Test Summary:
+            \(self.packets.count) packet(s) received
+            First received packet at frame \(self.firstPacketFrame ?? -1)
+            Average BER: \(self.averageBER ?? 1.0)
+            BER: \(self.packets.map{$0.BER})
+            """)
+    }
+}
+
 class InfoLED_ScannerTests: XCTestCase {
     var internalHistoryLens: [HistoryLens]!
     
@@ -55,12 +106,33 @@ class InfoLED_ScannerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+
+    func test0305() {
+        let testFolderName = "03-05"
+        let cameraConfigs = [2]
+        let cameraConfigNames = cameraConfigs.map{"\($0)x"}
+        let videoDistances = cameraConfigs.map{stride(from: 50 * $0, to: 0 * $0, by: -10 * $0)}
+        let videoDistanceNames = videoDistances.map{$0.map{"\($0)cm"}}
+        var resultDict = [String: TestVideoResult]()
+        for (cameraConfigName, videoNames) in zip(cameraConfigNames, videoDistanceNames) {
+            let directory = "\(testFolderName)/\(cameraConfigName)"
+            for videoName in videoNames {
+                print("\(cameraConfigName)/\(videoName)")
+                let result = testVideo(directory: directory, name: videoName)
+                resultDict["\(cameraConfigName)/\(videoName)"] = result
+            }
+        }
+        for (name, result) in resultDict {
+            print("Test \(name):\n")
+            result.printDescription()
+        }
+    }
     
-    func testExample() {
+    func testVideo(directory: String, name: String) -> TestVideoResult {
         let delegate = TestBufferProcessorDelegate()
         let eventLogger = MemoryEventLogger()
         let bufferProcessor = SampleBufferProcessor(delegate: delegate, eventLogger: eventLogger)
-        let videoURL = Bundle(for: type(of: self)).url(forResource: "10cm", withExtension: "MOV", subdirectory: "03-05/1x/")!
+        let videoURL = Bundle(for: type(of: self)).url(forResource: name, withExtension: "MOV", subdirectory: directory)!
         let videoAsset = AVAsset(url: videoURL)
         let reader = try! AVAssetReader(asset: videoAsset)
 
@@ -73,8 +145,7 @@ class InfoLED_ScannerTests: XCTestCase {
         reader.startReading()
 
         var lastEventCount = 0
-        var firstCorrectPacketFrame: Int?
-        var BER = [Float]()
+        let videoResult = TestVideoResult()
 
         while let sampleBuffer = trackReaderOutput.copyNextSampleBuffer() {
             bufferProcessor.processSampleBufferSync(sampleBuffer: sampleBuffer)
@@ -83,28 +154,20 @@ class InfoLED_ScannerTests: XCTestCase {
                 print("event: \(eventLogger.events[eventId])")
                 let currentEvent = eventLogger.events[eventId]
                 if currentEvent.message.keys.contains("decodedPacket") {
-                    let correctPackets = [0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1]
-                    let actualPackets = Array((currentEvent.message["decodedPacket"] as! [Int]).dropFirst(2))
-                    print(actualPackets)
-                    var wrongBits = 0
-                    for (actualDigit, correctDigit) in zip(actualPackets, correctPackets) {
-                        wrongBits += actualDigit == correctDigit ? 0 : 1
-                    }
-                    BER.append(Float(wrongBits) / Float(correctPackets.count))
-                    if wrongBits == 0 && firstCorrectPacketFrame == nil {
-                        firstCorrectPacketFrame = delegate.frameCount
-                    }
+                    var correctPackets = [0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1]
+                    let actualPacket = Array((currentEvent.message["decodedPacket"] as! [Int]).dropFirst(2))
+                    print(actualPacket)
+                    let packetResult = TestPacketResult(frameId: delegate.frameCount - 1, actualPacket: actualPacket, correctPacket: &correctPackets)
+                    videoResult.packets.append(packetResult)
                 }
             }
             lastEventCount = eventLogger.events.count
+            if delegate.frameCount >= 2400 {
+                break;
+            }
         }
-        print("""
-            Test Summary:
-            \(BER.count) packet(s) received
-            First received packet at frame \(firstCorrectPacketFrame ?? -1)
-            Average BER: \(Float(BER.reduce(0, +)) / Float(BER.count))
-            BER: \(BER)
-            """)
+        videoResult.calculateResult(totalFrames: delegate.frameCount)
+        return videoResult
     }
     
     func testPerformanceExample() {
